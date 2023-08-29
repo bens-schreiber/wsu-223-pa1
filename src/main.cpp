ac6996e8de78cc9abf0f50f3777d769b20855ed7
benjamin schreiber <bpschreiber2003@gmail.com>
2023-08-29 11:15:18 -0700

    advantage disadvantage linked list comment

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include "consts.hpp"
 #include "test/test.hpp"
 
+
+/* 
+
+    ADVANTAGE / DISADVANTAGE LINKED LIST:
+    When using a linked list, you get constant insertion time, works fine for the act of
+    loading the CSV file, and inserting new items. However, when you want to access/remove an item of a specific
+    index, or id, you have to iterate through the list, which is linear time.
+
+*/
+
 int main()
 {
     if (RUN_TESTS)
